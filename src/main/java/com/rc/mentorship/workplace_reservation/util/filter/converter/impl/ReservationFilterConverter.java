2a1454f2ea9971bd2689f0ec7106e5928080b023
Evgeniy Yurov <jon172839@gmail.com>
2024-07-23 01:00:35 +0300

    Refactor ReservationFilterConverter and add filtration by user and workplace for Reservations

--- src/main/java/com/rc/mentorship/workplace_reservation/util/filter/converter/impl/ReservationFilterConverter.java
+++ src/main/java/com/rc/mentorship/workplace_reservation/util/filter/converter/impl/ReservationFilterConverter.java
@@ -3,42 +3,44 @@ package com.rc.mentorship.workplace_reservation.util.filter.converter.impl;
 import com.rc.mentorship.workplace_reservation.entity.Reservation;
 import com.rc.mentorship.workplace_reservation.exception.FiltrationParamsFormatException;
 import com.rc.mentorship.workplace_reservation.util.filter.Filter;
-import com.rc.mentorship.workplace_reservation.util.filter.FilterType;
 import com.rc.mentorship.workplace_reservation.util.filter.converter.FilterToPredicateConverter;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 @Component
 public class ReservationFilterConverter implements FilterToPredicateConverter<Reservation> {
     @Override
     public Predicate<Reservation> convert(Map<String, Filter> filters) {
-        Filter startDateTimeFilter = filters.get("startDateTime");
-        Filter endDateTimeFilter = filters.get("endDateTime");
         List<Predicate<Reservation>> predicates = new ArrayList<>();
-        if (startDateTimeFilter != null) {
-            LocalDateTime startDateTime =
-                    LocalDateTime.parse(startDateTimeFilter.getValue());
-            predicates.add(filterStartDateTime(startDateTime,
-                    startDateTimeFilter.getType()));
-        }
-        if (endDateTimeFilter != null) {
-            LocalDateTime endDateTime =
-                    LocalDateTime.parse(endDateTimeFilter.getValue());
-            predicates.add(filterEndDateTime(endDateTime,
-                    endDateTimeFilter.getType()));
-        }
+        predicates.add(filterByStartDateTime(filters.get("startDateTime")));
+        predicates.add(filterByEndDateTime(filters.get("endDateTime")));
+        predicates.add(filterByUserId(filters.get("userId")));
+        predicates.add(filterByWorkplaceId(filters.get("workplaceId")));
 
+        predicates.removeIf(Objects::isNull);
         return applyAllPredicates(predicates);
     }
 
-    private Predicate<Reservation> filterStartDateTime(LocalDateTime startDateTime,
-                                                       FilterType filterType) {
-        switch (filterType) {
+    private Predicate<Reservation> filterByStartDateTime(Filter startDateTimeFilter) {
+        if (startDateTimeFilter == null) {
+            return null;
+        }
+
+        LocalDateTime startDateTime;
+        try {
+            startDateTime = LocalDateTime.parse(startDateTimeFilter.getValue());
+        } catch (DateTimeParseException ex) {
+            throw new FiltrationParamsFormatException("startDateTime");
+        }
+
+        switch (startDateTimeFilter.getType()) {
             case GREATER_THAN -> {
                 return reservation -> reservation.getStartDateTime().isAfter(startDateTime);
             }
@@ -60,9 +62,19 @@ public class ReservationFilterConverter implements FilterToPredicateConverter<Re
         }
     }
 
-    private Predicate<Reservation> filterEndDateTime(LocalDateTime endDateTime,
-                                                     FilterType filterType) {
-        switch (filterType) {
+    private Predicate<Reservation> filterByEndDateTime(Filter endDateTimeFilter) {
+        if (endDateTimeFilter == null) {
+            return null;
+        }
+
+        LocalDateTime endDateTime;
+        try {
+            endDateTime = LocalDateTime.parse(endDateTimeFilter.getValue());
+        } catch (DateTimeParseException ex) {
+            throw new FiltrationParamsFormatException("endDateTime");
+        }
+
+        switch (endDateTimeFilter.getType()) {
             case GREATER_THAN -> {
                 return reservation -> reservation.getEndDateTime().isAfter(endDateTime);
             }
@@ -82,5 +94,23 @@ public class ReservationFilterConverter implements FilterToPredicateConverter<Re
             }
             default -> throw new FiltrationParamsFormatException("endDateTime");
         }
+    }
+
+    private Predicate<Reservation> filterByUserId(Filter userIdFilter) {
+        if (userIdFilter == null) {
+            return null;
+        }
+
+        return reservation -> reservation.getUser().getId()
+                .toString().equals(userIdFilter.getValue());
+    }
+
+    private Predicate<Reservation> filterByWorkplaceId(Filter workplaceIdFilter) {
+        if (workplaceIdFilter == null) {
+            return null;
+        }
+
+        return reservation -> reservation.getWorkplace().getId()
+                .toString().equals(workplaceIdFilter.getValue());
     }
 }
