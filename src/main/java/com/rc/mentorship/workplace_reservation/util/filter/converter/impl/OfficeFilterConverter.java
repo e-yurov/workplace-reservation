ae7319b0913c332b10d5365a5c2432ec782123cd
Evgeniy Yurov <jon172839@gmail.com>
2024-07-22 19:21:50 +0300

    Change multiple predicates applying

--- src/main/java/com/rc/mentorship/workplace_reservation/util/filter/converter/impl/OfficeFilterConverter.java
+++ src/main/java/com/rc/mentorship/workplace_reservation/util/filter/converter/impl/OfficeFilterConverter.java
@@ -1,12 +1,15 @@
 package com.rc.mentorship.workplace_reservation.util.filter.converter.impl;
 
 import com.rc.mentorship.workplace_reservation.entity.Office;
+import com.rc.mentorship.workplace_reservation.exception.FiltrationParamsFormatException;
 import com.rc.mentorship.workplace_reservation.util.filter.Filter;
 import com.rc.mentorship.workplace_reservation.util.filter.FilterType;
 import com.rc.mentorship.workplace_reservation.util.filter.converter.FilterToPredicateConverter;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 
@@ -16,18 +19,17 @@ public class OfficeFilterConverter implements FilterToPredicateConverter<Office>
     public Predicate<Office> convert(Map<String, Filter> filters) {
         Filter startTimeFilter = filters.get("startTime");
         Filter endTimeFilter = filters.get("endTime");
-        Predicate<Office> result = null;
+        List<Predicate<Office>> predicates = new ArrayList<>();
         if (startTimeFilter != null) {
              LocalTime startTime = LocalTime.parse(startTimeFilter.getValue());
-             result = filterStartTime(startTime, startTimeFilter.getType());
+             predicates.add(filterStartTime(startTime, startTimeFilter.getType()));
         }
         if (endTimeFilter != null) {
             LocalTime endTime = LocalTime.parse(endTimeFilter.getValue());
-            var filteredEnd = filterEndTime(endTime, endTimeFilter.getType());
-            result = result == null ? filteredEnd : result.and(filteredEnd);
+            predicates.add(filterEndTime(endTime, endTimeFilter.getType()));
         }
 
-        return result;
+        return applyAllPredicates(predicates);
     }
 
     private Predicate<Office> filterStartTime(LocalTime startTime, FilterType filterType) {
@@ -49,8 +51,8 @@ public class OfficeFilterConverter implements FilterToPredicateConverter<Office>
             case EQUALS -> {
                 return office -> office.getStartTime().equals(startTime);
             }
+            default -> throw new FiltrationParamsFormatException();
         }
-        return null;
     }
 
     private Predicate<Office> filterEndTime(LocalTime endTime, FilterType filterType) {
@@ -72,7 +74,7 @@ public class OfficeFilterConverter implements FilterToPredicateConverter<Office>
             case EQUALS -> {
                 return office -> office.getEndTime().equals(endTime);
             }
+            default -> throw new FiltrationParamsFormatException();
         }
-        return null;
     }
 }
