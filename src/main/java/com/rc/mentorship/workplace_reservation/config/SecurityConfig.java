12ca9b25ec63b75f1bbbeb3831c7edca39a6df9c
Evgeniy Yurov <jon172839@gmail.com>
2024-09-02 02:26:50 +0300

    Add Prometheus and Grafana

--- src/main/java/com/rc/mentorship/workplace_reservation/config/SecurityConfig.java
+++ src/main/java/com/rc/mentorship/workplace_reservation/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/swagger-ui/index.html").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
         );
         http.sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
